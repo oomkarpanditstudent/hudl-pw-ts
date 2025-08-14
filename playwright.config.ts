96459053a764f9eb795995ba206face53a6fd553
Oomkar Pandit <lolasoftwaretesting@gmail.com>
2025-08-14 19:19:27 +0100

    ci: experiments with ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,9 +17,18 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 3 : undefined,
+  workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? [['junit', { outputFile: 'results.xml' }], ['html']] : 'html',
+  reporter: process.env.CI
+    ? [
+        ['dot'], // fast console output for CI
+        ['junit', { outputFile: 'results.xml' }], 
+        ['html', { open: 'never' }] 
+      ]
+    : [
+        ['list'], // prettier local console output
+        ['html']  // local HTML report auto-open
+      ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
