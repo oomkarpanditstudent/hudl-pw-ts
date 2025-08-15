0ded35e50ba4725a19d54cd7e1813c23e705e138
Oomkar Pandit <lolasoftwaretesting@gmail.com>
2025-08-15 16:43:21 +0100

    ci: setting up CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,9 +17,9 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? [['junit', { outputFile: 'results.xml' }], ['html']] : 'html',
+  reporter: process.env.CI ? [['junit', { outputFile: 'results.xml' }], ['html', { open: 'never' }]] : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -28,7 +28,7 @@ export default defineConfig({
     screenshot: 'on',
     video: 'retain-on-failure',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
   },
 
   /* Configure projects for major browsers */
